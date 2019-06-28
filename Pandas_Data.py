beb8839e979bb20e857e7deba44592c164bc2c5c
Alessandro Muroni <muronialessandro@gmail.com>
2019-06-28 14:35:52 +0200

    numpy and pandas exercises

--- Pandas_Data.py
+++ Pandas_Data.py
@@ -17,4 +17,10 @@ area = pd.Series({'Alaska': 1723337, 'Texas': 695662,
                   'California': 423967}, name='area')
 population = pd.Series({'California': 38332521, 'Texas': 26448193,
                         'New York': 19651127}, name='population')
-print(population/area) # NY is NaN
+print(population/area)  # NY is NaN
+# another NaN example
+A = pd.Series([2, 4, 6], index=[0, 1, 2])
+B = pd.Series([1, 3, 5], index=[1, 2, 3])
+print(A + B)  # shows NaN in 0 and 3
+print(A.add(B, fill_value=0))  # in this case it will sum with 0 as filler for NaN value
+
